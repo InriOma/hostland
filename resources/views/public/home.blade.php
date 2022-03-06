a101d54c92fccf0a611f45c6f230cd61d9f46b74
Inri Zurita <inrioma@gmail.com>
2022-03-06 01:34:09 -0600

    bug mostrar slider

--- resources/views/public/home.blade.php
+++ resources/views/public/home.blade.php
@@ -5,23 +5,28 @@
 
 <div id="carouselExampleDark" class="carousel  slide" data-bs-ride="carousel">
   <div class="carousel-indicators">
-    @foreach ($homes as $home )
+    @if(isset($homes))
+      @foreach ($homes as $home )
         <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="{{ $loop->iteration-1 }}" class="active" aria-current="true" aria-label="Slide {{ $loop->iteration }}"></button>
-    @endforeach
+      @endforeach
+    @endif
   </div>
 
   <div class="carousel-inner">
-  
-    @foreach ($homes as $home )
-      <div class="carousel-item @if($loop->first) active @endif" data-bs-interval="10000">
-        <img src="{{ $home->url.$home->nameImage }}" class="d-block w-100" alt="...">
-        <div class="carousel-caption d-none d-md-block">
-          <h5 class="text-white">{{ $home->title }} </h5>
-          <p class="text-white">{{ $home->description }}</p>
+    @if(isset($homes))
+      @foreach ($homes as $home )
+        <div class="carousel-item @if($loop->first) active @endif" data-bs-interval="10000">
+          <img src="{{ $home->url.$home->nameImage }}" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h5 class="text-white">{{ $home->title }} </h5>
+            <p class="text-white">{{ $home->description }}</p>
+          </div>
         </div>
-      </div>
-    @endforeach
- 
+      @endforeach
+    @else
+    <h1>No hay Slider para mostrar</h1>
+    @endif
+
   </div>
 
   <button class="carousel-control-prev text-white" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
